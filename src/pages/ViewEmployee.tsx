e933477f891d8bc202bea6050cb967de6535d36e
giannischiout <giannis.chiout@gmail.com>
2026-02-22 21:34:43 +0200

    (feat): display basic details and add some styling

--- src/pages/ViewEmployee.tsx
+++ src/pages/ViewEmployee.tsx
@@ -10,11 +10,21 @@ export default function ViewEmployee() {
       id: params.id ?? ""
     },
   })
-  console.log({data})
+  const employee = data?.[0]
   return (
-    <div>
-      {params.id}
-
-    </div>
+    <section className="employee_view_container" >
+    
+      <div className="card">
+      <h2>Employee Details:</h2>
+         <div>
+           <span>Full Name</span>
+           <p>{`${employee?.firstName} ${employee?.lastName} `}</p>
+         </div>
+         <div>
+           <span>Full Name</span>
+           <p>{`${employee?.firstName} ${employee?.lastName} `}</p>
+         </div>
+      </div>
+    </section>
   )
 }
